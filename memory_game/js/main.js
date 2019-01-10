1c5ea20679c3406a2dea2e9af82d1b949fdc9396
Peter Fitzpatrick <peterfitzpatrick@Peters-MBP.attlocal.net>
2019-01-10 09:22:37 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,21 @@
 //console.log("Up and running!");
-var cardOne = 'queen';
-var cardTwo = 'queen';
-var cardThree = 'king';
-var cardFour = 'king';
+cards = ['queen', 'queen', 'king', 'king'];
+cardsInPlay = [];
 
-console.log("User flipped " + cardOne + " and " + cardThree + ".");
+cardOne = cards[0];
+cardTwo = cards[1];
+
+cardsInPlay.push(cardOne);
+console.log("User flipped " + cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User flipped " + cardTwo);
+
+if (cardsInPlay.length === 2) {
+  console.log('Player has selected 2 cards');
+} if (cardsInPlay[0] === cardsInPlay[1]) {
+  alert('You have a match!');
+} else if (cardsInPlay[0] !== cardsInPlay[1]) {
+  alert('Sorry, try again.');
+} else {
+  console.log('Only 1 card has been selected');
+};
